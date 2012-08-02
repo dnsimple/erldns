43f43d74f5d2f5a6a3950f0561efcede8aaf894a
Anthony Eden <anthonyeden@gmail.com>
2012-08-02 23:50:45 +0200

    Making mysql work as a data store.

--- src/erldns_server.erl
+++ src/erldns_server.erl
@@ -22,6 +22,8 @@
 start() ->
   start(?PORT).
 start(Port) ->
+  application:start(mysql),
+
   spawn(fun() -> udp_server(Port) end),
   spawn(fun() -> tcp_server(Port) end).
 
@@ -79,7 +81,6 @@ handle_dns_query(Socket, Packet) ->
   io:format("TCP Message received, len: ~p~n", [Len]),
   DecodedMessage = dns:decode_message(Bin),
   NewResponse = answer_questions(DecodedMessage#dns_message.questions, DecodedMessage),
-  %Response = build_response(Request),
   BinReply = dns:encode_message(NewResponse),
   BinLength = byte_size(BinReply),
   TcpBinReply = <<BinLength:16, BinReply/binary>>,
@@ -89,13 +90,10 @@ handle_dns_query(Socket, Packet) ->
 %% Handle DNS query that comes in over UDP
 handle_dns_query(Socket, Host, Port, Bin) ->
   io:format("Message from from ~p~n", [Host]),
-
   DecodedMessage = dns:decode_message(Bin),
   io:format("Decoded message ~p~n", [DecodedMessage]),
   NewResponse = answer_questions(DecodedMessage#dns_message.questions, DecodedMessage),
   BinReply = dns:encode_message(NewResponse),
-
-  %Response = build_response(Request),
   gen_udp:send(Socket, Host, Port, BinReply).
 
 %% Answer the questions and return an updated copy of the given
@@ -111,7 +109,7 @@ answer_questions([Q|Rest], Response) ->
 %% an updated copy of the Response.
 answer_question(Q, Response) ->
   [Name, Type] = [Q#dns_query.name, Q#dns_query.type],
-  Answers = erldns_fake_responder:answer(Name, dns:type_name(Type)),
+  Answers = erldns_mysql_responder:answer(Name, dns:type_name(Type)),
   NewResponse = Response#dns_message{anc = length(Answers), aa = true, answers = Answers},
-  io:format("New response: ~p~n", [NewResponse]),
+  io:format("Response: ~p~n", [NewResponse]),
   NewResponse.
