a313c591c01e38fd6674541cf5fc7bdc3af466ca
Anthony Eden <anthonyeden@gmail.com>
2012-09-07 11:22:01 +0200

    Remove unnecessary comment.

--- src/erldns_app.erl
+++ src/erldns_app.erl
@@ -4,8 +4,6 @@
 % Application hooks
 -export([start/2, stop/1]).
 
-%%% Application, this is a work-in-progress
-
 start(Type, Args) ->
   lager:info("~p:start(~p, ~p)~n", [?MODULE, Type, Args]),
   erldns_sup:start_link().
