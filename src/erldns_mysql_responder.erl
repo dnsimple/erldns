c96a4f9d42a75e7e646d7ac1bbef0b41d7cfef22
Anthony Eden <anthonyeden@gmail.com>
2012-09-12 22:10:33 +0200

    Resolve CNAME records to their A names if requested with an A or ANY type in the question. If the question is a CNAME type then only return the CNAME record.

--- src/erldns_mysql_responder.erl
+++ src/erldns_mysql_responder.erl
@@ -7,15 +7,11 @@
 
 answer(Qname, Qtype) ->
   lager:debug("~p:answer(~p, ~p)~n", [?MODULE, Qname, Qtype]),
-  lists:flatten(lists:map(
-    fun(Row) ->
-      [_, _Id, Name, TypeStr, Content, TTL, Priority, _ChangeDate] = Row,
-      case parse_content(Content, Priority, TypeStr) of
-        unsupported -> [];
-        Data -> #dns_rr{name=Name, type=erldns_records:name_type(TypeStr), data=Data, ttl=TTL}
-      end
-    end, lookup(Qname, Qtype)
-  )).
+  Records = lists:flatten(lists:map(fun row_to_record/1, lookup(Qname, Qtype))),
+  case Qtype of
+    ?DNS_TYPE_CNAME_BSTR -> Records;
+    _ -> resolve_cnames(Records)
+  end.
 
 lookup(Qname, Qtype) ->
   lager:debug("~p:lookup(~p, ~p)~n", [?MODULE, Qname, Qtype]),
@@ -23,12 +19,36 @@ lookup(Qname, Qtype) ->
     ?DNS_TYPE_ANY_BSTR ->
       mysql:prepare(select_records, <<"select * from records where name = ?">>),
       mysql:execute(dns_pool, select_records, [Qname]);
+    ?DNS_TYPE_A_BSTR ->
+      mysql:prepare(select_records_of_a_type, <<"select * from records where name = ? and (type = ? or type = ?)">>),
+      mysql:execute(dns_pool, select_records_of_a_type, [Qname, Qtype, <<"CNAME">>]);
     _ ->
       mysql:prepare(select_records_of_type, <<"select * from records where name = ? and type = ?">>),
       mysql:execute(dns_pool, select_records_of_type, [Qname, Qtype])
   end,
   lager:debug("~p:lookup found rows~n", [?MODULE]),
   Data#mysql_result.rows.
+
+resolve_cnames(Records) ->
+  [resolve_cname(Record) || Record <- Records].
+
+resolve_cname(Record) ->
+  lager:debug("~p:resolve_cname(~p)~n", [?MODULE, Record]),
+  case Record#dns_rr.type of
+    ?DNS_TYPE_CNAME_NUMBER ->
+      [Qname, Qtype] = [Record#dns_rr.data#dns_rrdata_cname.dname, ?DNS_TYPE_A_BSTR],
+      lists:map(fun row_to_record/1, lookup(Qname, Qtype)) ++ [Record];
+    _ ->
+      Record
+  end.
+
+
+row_to_record(Row) ->
+  [_, _Id, Name, TypeStr, Content, TTL, Priority, _ChangeDate] = Row,
+  case parse_content(Content, Priority, TypeStr) of
+    unsupported -> [];
+    Data -> #dns_rr{name=Name, type=erldns_records:name_type(TypeStr), data=Data, ttl=TTL}
+  end.
 
 %% All of these functions are used to parse the content field
 %% stored in MySQL into a correct dns_rrdata in-memory record.
