bf7b6b24772652b5e99d93a05010afd737ae84eb
Anthony Eden <anthonyeden@gmail.com>
2012-09-13 09:25:20 +0200

    Document why this CNAME resolution occurs. I am considering moving this up the call chain a bit so it occurs *after* all of the handlers have run.

--- src/erldns_mysql_responder.erl
+++ src/erldns_mysql_responder.erl
@@ -8,6 +8,14 @@
 answer(Qname, Qtype) ->
   lager:debug("~p:answer(~p, ~p)~n", [?MODULE, Qname, Qtype]),
   Records = lists:flatten(lookup(Qname, Qtype)),
+
+  %% According to RFC 1034:
+  %%
+  %% "CNAME RRs cause special action in DNS software.  When a name server fails to find a desired RR 
+  %% in the resource set associated with the domain name, it checks to see if the resource set consists 
+  %% of a CNAME record with a matching class.  If so, the name server includes the CNAME record in the 
+  %% response and restarts the query at the domain name specified in the data field of the CNAME record.  
+  %% The one exception to this rule is that queries which match the CNAME type are not restarted."
   case Qtype of
     ?DNS_TYPE_CNAME_BSTR -> Records;
     _ -> [resolve_cname(Record) || Record <- Records]
