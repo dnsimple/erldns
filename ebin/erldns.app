2e47901c488f854670efd6c9ecd05a53ff0086eb
Anthony Eden <anthonyeden@gmail.com>
2012-09-24 13:33:29 -0500

    Set up debugging for processes, but shut it off for now.

--- ebin/erldns.app
+++ ebin/erldns.app
@@ -1,6 +1,6 @@
 {application,erldns,
              [{description,"Erlang Authoritative DNS Server"},
-              {vsn,"9662073"},
+              {vsn,"0358d58"},
               {modules,[erldns,erldns_app,erldns_axfr,erldns_debugging,
                         erldns_dnssec,erldns_edns,erldns_encoder,
                         erldns_handler,erldns_mysql_responder,
--- src/erldns_debugging.erl
+++ src/erldns_debugging.erl
@@ -2,6 +2,8 @@
 
 -export([start/0]).
 
+-define(MIN_PROCESS_COUNT, 500).
+
 start() ->
   timer:start(),
   spawn(fun() -> loop() end).
@@ -11,5 +13,9 @@ loop() ->
 
 loop(IterationNumber) ->
   lager:info("Iteration ~p (processes: ~p)", [IterationNumber, length(erlang:processes())]),
+  case length(erlang:processes()) of
+    N when N > ?MIN_PROCESS_COUNT -> lager:info("~p", [lists:map(fun erlang:process_info/1, erlang:processes())]);
+    _ -> ok
+  end,
   timer:sleep(10000),
   loop(IterationNumber + 1).
