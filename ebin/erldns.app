fa7a55c6507ade138ee118de5367770f6caff7e2
Anthony Eden <anthonyeden@gmail.com>
2012-09-24 20:05:16 -0500

    Write to ets if we hit +500 processes.

--- ebin/erldns.app
+++ ebin/erldns.app
@@ -1,6 +1,6 @@
 {application,erldns,
              [{description,"Erlang Authoritative DNS Server"},
-              {vsn,"15741a3"},
+              {vsn,"d8a3357"},
               {modules,[erldns,erldns_app,erldns_axfr,erldns_debugging,
                         erldns_dnssec,erldns_edns,erldns_encoder,
                         erldns_handler,erldns_mysql_responder,
--- src/erldns_debugging.erl
+++ src/erldns_debugging.erl
@@ -6,7 +6,8 @@
 
 start() ->
   timer:start(),
-  spawn_link(fun() -> loop() end).
+  ets:new(precrash_process_info, [set, named_table]),
+  spawn(fun() -> loop() end).
 
 loop() ->
   loop(1).
@@ -14,7 +15,7 @@ loop() ->
 loop(IterationNumber) ->
   lager:info("Iteration ~p (processes: ~p)", [IterationNumber, length(erlang:processes())]),
   case length(erlang:processes()) of
-    N when N > ?MIN_PROCESS_COUNT -> lager:info("~p", [lists:map(fun erlang:process_info/1, erlang:processes())]);
+    N when N > ?MIN_PROCESS_COUNT -> ets:insert(precrash_process_info, [lists:map(fun erlang:process_info/1, erlang:processes())]);
     _ -> ok
   end,
   timer:sleep(10000),
