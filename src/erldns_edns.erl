a5181084bc8e5b2ab1fe7f7a1ac4301328f77eb9
Anthony Eden <anthonyeden@gmail.com>
2012-10-22 18:54:49 +0200

    Only try to process the dns_optrr if the additional record set is actually a dns_optrr record.

--- src/erldns_edns.erl
+++ src/erldns_edns.erl
@@ -9,9 +9,12 @@ handle(Message) ->
   
 handle_opts(Message, []) ->
   Message;
-handle_opts(Message, [Opt|Rest]) ->
-  NewMessage = case Opt#dns_optrr.dnssec of
+handle_opts(Message, [RR|Rest]) when is_record(RR, dns_optrr) ->
+  NewMessage = case RR#dns_optrr.dnssec of
     true -> erldns_dnssec:handle(Message);
     false -> Message
   end,
-  handle_opts(NewMessage, Rest).
+  handle_opts(NewMessage, Rest);
+handle_opts(Message, [RR|Rest]) ->
+  lager:info("~p:handle_opts(~p, [~p|Rest])", [?MODULE, Message, RR]),
+  handle_opts(Message, Rest).
