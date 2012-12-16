538be57b4ba28399802ae18833c12c00b604f7cc
Anthony Eden <anthonyeden@gmail.com>
2012-12-16 13:23:39 +0100

    Query the records table using the domain id.
    Removes unnecessary functions and simplifies queries.

--- src/erldns_pgsql.erl
+++ src/erldns_pgsql.erl
@@ -3,7 +3,7 @@
 -include("dns.hrl").
 -include("erldns.hrl").
 
--export([init/0, lookup_name/3, lookup_records/0, lookup_records/1, domain_names/1, equery/2, equery/3]).
+-export([init/0, lookup_records/0, domain_names/1]).
 
 init() -> ok.
 
@@ -11,54 +11,21 @@ lookup_records() ->
   case squery("select * from domains") of
     {ok, _, Rows} ->
       lists:map(
-        fun({_,Name,_,_,_,_,_}) ->
-          {Name, lookup_records(Name)}
+        fun({Id,Name,_,_,_,_,_}) ->
+            {Name, lookup_records(Name, list_to_integer(binary_to_list(Id)))}
         end, Rows);
     Result ->
       lager:error("~p:~p", [?MODULE, Result]), []
   end.
 
-lookup_records(Qname) -> 
-  DomainNames = domain_names(Qname),
-  case equery(build_domain_records_query(DomainNames), DomainNames) of
+lookup_records(Name, DomainId) ->
+  lager:info("lookup_records(~p, ~p)", [Name, DomainId]),
+  case equery("select * from records where domain_id = $1", [DomainId]) of
     {ok, _, Rows} ->
-      lists:map(fun(Row) -> row_to_record(Qname, Row) end, Rows);
+      lists:map(fun(Row) -> row_to_record(Name, Row) end, Rows);
     Result ->
       lager:error("~p:~p", [?MODULE, Result]), []
   end.
-
-build_domain_records_query(DomainNames) ->
-  {WhereClause, _} = build_domain_list_clause(DomainNames),
-  list_to_binary(["select records.* from domains join records on domains.id = records.domain_id where domains.id = (select records.domain_id from records where "] ++ [WhereClause, " limit 1)"]).
-
-lookup_name(Qname, Qtype, LookupName) ->
-  lager:debug("~p:lookup_name(~p, ~p, ~p)", [?MODULE, Qname, Qtype, LookupName]),
-  QueryResult = case Qtype of
-    ?DNS_TYPE_AXFR_BSTR ->
-      equery(<<"select records.* from domains join records on domains.id = records.domain_id where lower(domains.name) = $1">>, [Qname]);
-    ?DNS_TYPE_ANY_BSTR ->
-      equery(<<"select * from records where lower(name) = $1">> , [LookupName]);
-    _ ->
-      equery(pgsql_pool, <<"select * from records where lower(name) = $1 and type = $2">>, [LookupName, Qtype])
-  end,
-  case QueryResult of
-    {ok, _, Rows} ->
-      lists:map(fun(Row) -> row_to_record(Qname, Row) end, Rows);
-    Result ->
-      lager:error("~p:~p", [?MODULE, Result]), []
-  end.
-
-%% Given a list of domain names, build an OR-separated SQL where
-%% clause with the appropriate placeholders. Return the result
-%% as the tuple {WhereClause, NextIndex}.
-build_domain_list_clause(DomainNames) ->
-  build_domain_list_clause(DomainNames, 1).
-build_domain_list_clause(DomainNames, Offset) ->
-  build_domain_list_clause(DomainNames, Offset, []).
-build_domain_list_clause([], Index, Clauses) ->
-  {string:join(Clauses, " or "), Index};
-build_domain_list_clause([_|Rest], Index, Clauses) ->
-  build_domain_list_clause(Rest, Index + 1, Clauses ++ [lists:concat(["lower(name) = $", Index])]).
 
 %% Convert a name to a list of possible domain names by working
 %% back through the labels to construct each possible domain.
