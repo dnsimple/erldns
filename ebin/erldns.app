ef5d78ee8eb6d3f73d8629c6e568d071e6293774
Anthony Eden <anthonyeden@gmail.com>
2013-05-27 15:52:38 +0200

    Pass in the module name for the Modules argument in case it is necessary.

--- ebin/erldns.app
+++ ebin/erldns.app
@@ -1,6 +1,6 @@
 {application,erldns,
              [{description,"Erlang Authoritative DNS Server"},
-              {vsn,"6f8a997"},
+              {vsn,"a24c735"},
               {mod,{erldns_app,[]}},
               {applications,[kernel,stdlib,inets,crypto,ssl]},
               {start_phases,[{post_start,[]}]},
--- src/erldns_websocket_sup.erl
+++ src/erldns_websocket_sup.erl
@@ -17,7 +17,7 @@ init(_Args) ->
   WebsocketUrl = erldns_zone_client:websocket_url(),
   lager:debug("Connecting to web socket: ~p", [WebsocketUrl]),
   Procs = [
-    {websocket_client, {websocket_client, start_link, [WebsocketUrl, erldns_zone_client, []]}, permanent, 5000, worker, []}
+    {websocket_client, {websocket_client, start_link, [WebsocketUrl, erldns_zone_client, []]}, permanent, 5000, worker, [websocket_client]}
   ],
 
   {ok, {{one_for_one, 20, 10}, Procs}}.
