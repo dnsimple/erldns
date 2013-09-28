39de33475478c462f208c4662be9c2750575f1fb
Anthony Eden <anthonyeden@gmail.com>
2013-09-28 17:55:50 +0200

    Provide a start_link implementation that takes an empty list.

--- src/erldns_zone_fetcher.erl
+++ src/erldns_zone_fetcher.erl
@@ -17,12 +17,14 @@
 
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([start_link/0, start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {}).
 
 start_link() ->
+  start_link([]).
+start_link([]) ->
   gen_server:start_link(?MODULE, [], []).
 
 init(_) ->
