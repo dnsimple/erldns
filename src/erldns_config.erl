49ee1aafb36284d77ac8300c7259086883fbf328
Anthony Eden <anthonyeden@gmail.com>
2013-10-11 13:09:07 +0200

    Docs and specs.

--- src/erldns_config.erl
+++ src/erldns_config.erl
@@ -22,7 +22,12 @@
 -define(DEFAULT_PORT, 53).
 -define(DEFAULT_NUM_WORKERS, 10).
 
-%% Private functions
+%% @doc Get the IP address (either IPv4 or IPv6) that the DNS server
+%% should listen on.
+%%
+%% IPv4 default: 127.0.0.1
+%% IPv6 default: ::1
+-spec get_address(inet | inet6) -> inet:ip_address().
 get_address(inet) ->
   case application:get_env(erldns, inet4) of
     {ok, Address} -> parse_address(Address);
@@ -34,17 +39,27 @@ get_address(inet6) ->
     _ -> ?DEFAULT_IPV6_ADDRESS
   end.
 
+%% @doc The the port that the DNS server should listen on.
+%%
+%% Default: 53
+-spec get_port() -> inet:port_number().
 get_port() ->
   case application:get_env(erldns, port) of
     {ok, Port} -> Port;
     _ -> ?DEFAULT_PORT
   end.
 
+%% @doc Get the number of workers to run for handling DNS requests.
+%%
+%% Default: 10
+-spec get_num_workers() -> non_neg_integer().
 get_num_workers() ->
   case application:get_env(erldns, num_workers) of
     {ok, NumWorkers} -> NumWorkers;
     _ -> ?DEFAULT_NUM_WORKERS
   end.
+
+% Private functions
 
 parse_address(Address) when is_list(Address) ->
   {ok, Tuple} = inet_parse:address(Address),
