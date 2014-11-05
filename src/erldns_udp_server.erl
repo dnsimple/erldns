38550ff5036c498f0fa885e397f5f214ddf51603
Anthony Eden <anthonyeden@gmail.com>
2014-11-05 09:40:56 -0500

    Update copyright date.

--- src/erldns_udp_server.erl
+++ src/erldns_udp_server.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2012-2013, Aetrion LLC
+%% Copyright (c) 2012-2014, Aetrion LLC
 %%
 %% Permission to use, copy, modify, and/or distribute this software for any
 %% purpose with or without fee is hereby granted, provided that the above
