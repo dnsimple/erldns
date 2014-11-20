166eef525477fe5b04f4876d58495c09c52760f2
Leonard Boyce <leonard@siftlogic.com>
2014-11-20 09:32:58 -0500

    (feat) backward compatible type definitions for specs

--- include/erldns.hrl
+++ include/erldns.hrl
@@ -1,3 +1,15 @@
+%% Erlang 17 moves various types into module namespaces
+%% Use the ifdef to be backward compatible
+-ifdef(namespaced_types).
+-type edns_set() :: sets:set().
+-type edns_dict() :: dict:dict().
+-type edns_gb_tree() :: gb_trees:gb_tree().
+-else.
+-type edns_set() :: set().
+-type edns_dict() :: dict().
+-type edns_gb_tree() :: gb_tree().
+-endif.
+
 -record(zone, {
     name :: dns:dname(),
     version :: binary(),
