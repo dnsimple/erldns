af060ad14be3528e8130a084320f9e2dbf4b98d2
Kyle Neal <kyle@verias.com>
2014-12-15 14:08:00 -0500

    (chore) #noticket Emacs reformatting, and code cleanup.

--- src/erldns_app.erl
+++ src/erldns_app.erl
@@ -16,62 +16,62 @@
 -module(erldns_app).
 -behavior(application).
 
-% Application hooks
+%% Application hooks
 -export([start/2, start_phase/3, stop/1]).
 
 start(_Type, _Args) ->
-  erldns_log:debug("Starting erldns application"),
-  setup_metrics(),
-  erldns_sup:start_link().
+    erldns_log:debug("Starting erldns application"),
+    setup_metrics(),
+    erldns_sup:start_link().
 
 start_phase(post_start, _StartType, _PhaseArgs) ->
-  erldns_log:debug("Post start phase for erldns application"),
-  erldns_events:add_handler(erldns_event_handler),
+    erldns_log:debug("Post start phase for erldns application"),
+    erldns_events:add_handler(erldns_event_handler),
 
-  erldns_log:debug("Loading custom zone parsers"),
-  case application:get_env(erldns, custom_zone_parsers) of
-    {ok, Parsers} -> erldns_zone_parser:register_parsers(Parsers);
-    _ -> ok
-  end,
+    erldns_log:debug("Loading custom zone parsers"),
+    case application:get_env(erldns, custom_zone_parsers) of
+        {ok, Parsers} -> erldns_zone_parser:register_parsers(Parsers);
+        _ -> ok
+    end,
 
-  erldns_log:debug("Loading custom zone encoders"),
-  case application:get_env(erldns, custom_zone_encoders) of
-    {ok, Encoders} -> erldns_zone_encoder:register_encoders(Encoders);
-    _ -> ok
-  end,
+    erldns_log:debug("Loading custom zone encoders"),
+    case application:get_env(erldns, custom_zone_encoders) of
+        {ok, Encoders} -> erldns_zone_encoder:register_encoders(Encoders);
+        _ -> ok
+    end,
 
-  erldns_log:info("Loading zones from local file"),
-  case erldns_config:storage_type() of
-  erldns_storage_json ->
-      erldns_storage:load_zones();
-  erldns_storage_mnesia ->
-      ok
-  end,
+    erldns_log:info("Loading zones from local file"),
+    case erldns_config:storage_type() of
+        erldns_storage_json ->
+            erldns_storage:load_zones();
+        erldns_storage_mnesia ->
+            ok
+    end,
 
-  erldns_log:info("Notifying servers to start"),
-  erldns_events:notify(start_servers),
-  ok.
+    erldns_log:info("Notifying servers to start"),
+    erldns_events:notify(start_servers),
+    ok.
 
 stop(_State) ->
-  erldns_log:info("Stop erldns application"),
-  ok.
+    erldns_log:info("Stop erldns application"),
+    ok.
 
 setup_metrics() ->
-  folsom_metrics:new_counter(udp_request_counter),
-  folsom_metrics:new_counter(tcp_request_counter),
-  folsom_metrics:new_meter(udp_request_meter),
-  folsom_metrics:new_meter(tcp_request_meter),
+    folsom_metrics:new_counter(udp_request_counter),
+    folsom_metrics:new_counter(tcp_request_counter),
+    folsom_metrics:new_meter(udp_request_meter),
+    folsom_metrics:new_meter(tcp_request_meter),
 
-  folsom_metrics:new_histogram(udp_handoff_histogram),
-  folsom_metrics:new_histogram(tcp_handoff_histogram),
+    folsom_metrics:new_histogram(udp_handoff_histogram),
+    folsom_metrics:new_histogram(tcp_handoff_histogram),
 
-  folsom_metrics:new_counter(request_throttled_counter),
-  folsom_metrics:new_meter(request_throttled_meter),
-  folsom_metrics:new_histogram(request_handled_histogram),
+    folsom_metrics:new_counter(request_throttled_counter),
+    folsom_metrics:new_meter(request_throttled_meter),
+    folsom_metrics:new_histogram(request_handled_histogram),
 
-  folsom_metrics:new_counter(packet_dropped_empty_queue_counter),
-  folsom_metrics:new_meter(packet_dropped_empty_queue_meter),
+    folsom_metrics:new_counter(packet_dropped_empty_queue_counter),
+    folsom_metrics:new_meter(packet_dropped_empty_queue_meter),
 
-  folsom_metrics:new_meter(cache_hit_meter),
-  folsom_metrics:new_meter(cache_expired_meter),
-  folsom_metrics:new_meter(cache_miss_meter).
+    folsom_metrics:new_meter(cache_hit_meter),
+    folsom_metrics:new_meter(cache_expired_meter),
+    folsom_metrics:new_meter(cache_miss_meter).
