3039a03a9b923b7cc3104b4136e3df5eec022723
Kyle Neal <kyle@verias.com>
2014-12-15 14:10:55 -0500

    (chore) #noticket Emacs reformatting, and code cleanup.

--- src/erldns_query_throttle.erl
+++ src/erldns_query_throttle.erl
@@ -22,9 +22,12 @@
 -include_lib("dns/include/dns_records.hrl").
 
 %% API
--export([start_link/0, throttle/2, sweep/0, stop/0]).
+-export([start_link/0,
+         throttle/2,
+         sweep/0,
+         stop/0]).
 
-% Gen server hooks
+%% Gen server hooks
 -export([init/1,
          handle_call/3,
          handle_cast/2,
@@ -48,88 +51,89 @@
 %% @doc Start the query throttle process.
 -spec start_link() -> any().
 start_link() ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 %% @doc Throttle the given message if necessary.
 -spec throttle(dns:message(), inet:ip_address() | inet:hostname()) -> ok | throttle_result().
 throttle(_Message, {tcp, _Host}) ->
-  ok;
+    ok;
 throttle(Message, {_, Host}) ->
-  case ?ENABLED of
-    true ->
-      case lists:filter(fun(Q) -> Q#dns_query.type =:= ?DNS_TYPE_ANY end, Message#dns_message.questions) of
-        [] -> ok;
-        _ -> record_request(maybe_throttle(Host))
-      end;
-    _ ->
-      %erldns_log:debug("Throttle not enabled"),
-      ok
-  end.
+    case ?ENABLED of
+        true ->
+            case lists:filter(fun(Q) -> Q#dns_query.type =:= ?DNS_TYPE_ANY end,
+                              Message#dns_message.questions) of
+                [] -> ok;
+                _ -> record_request(maybe_throttle(Host))
+            end;
+        _ ->
+            ok
+    end.
 
 %% @doc Sweep the query throttle table for expired host records.
 -spec sweep() -> any().
 sweep() ->
-  gen_server:cast(?MODULE, sweep).
+    gen_server:cast(?MODULE, sweep).
 
 %% @doc Stop the query throttle process normally.
 -spec stop() -> any().
 stop() ->
-  gen_server:call(?MODULE, stop).
+    gen_server:call(?MODULE, stop).
 
 
-% Gen server hooks
+%% Gen server hooks
 init([]) ->
-  erldns_storage:create(host_throttle),
-  {ok, Tref} = timer:apply_interval(?SWEEP_INTERVAL, ?MODULE, sweep, []),
-  {ok, #state{tref = Tref}}.
+    erldns_storage:create(host_throttle),
+    {ok, Tref} = timer:apply_interval(?SWEEP_INTERVAL, ?MODULE, sweep, []),
+    {ok, #state{tref = Tref}}.
 
 handle_call(stop, _From, State) ->
-  {stop, normal, ok, State}.
+    {stop, normal, ok, State}.
 
 handle_cast(sweep, State) ->
-  Keys = erldns_storage:select(host_throttle, [{{'$1', {'_', '$2'}}, [{'<', '$2', timestamp() - ?EXPIRATION}], ['$1']}]),
-  lists:foreach(fun(K) -> erldns_storage:delete(host_throttle, K) end, Keys),
-  {noreply, State}.
+    Keys = erldns_storage:select(host_throttle,
+                                 [{{'$1', {'_', '$2'}}, [{'<', '$2', timestamp() - ?EXPIRATION}], ['$1']}]),
+    lists:foreach(fun(K) -> erldns_storage:delete(host_throttle, K) end, Keys),
+    {noreply, State}.
 
 handle_info(_Message, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 terminate(_Reason, _State) ->
-  erldns_storage:delete_table(host_throttle),
-  ok.
+    erldns_storage:delete_table(host_throttle),
+    ok.
 
 code_change(_PreviousVersion, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
 
 
-% Internal API
+%% Internal API
 maybe_throttle(Host) ->
-  case erldns_storage:select(host_throttle, Host) of
-    [{_, {ReqCount, LastRequestAt}}] -> 
-      case is_throttled(Host, ReqCount, LastRequestAt) of
-        {true, NewReqCount} -> {throttled, Host, NewReqCount};
-        {false, NewReqCount} -> {ok, Host, NewReqCount}
-      end;
-    [] -> 
-      {ok, Host, 1}
-  end.
+    case erldns_storage:select(host_throttle, Host) of
+        [{_, {ReqCount, LastRequestAt}}] ->
+            case is_throttled(Host, ReqCount, LastRequestAt) of
+                {true, NewReqCount} -> {throttled, Host, NewReqCount};
+                {false, NewReqCount} -> {ok, Host, NewReqCount}
+            end;
+        [] ->
+            {ok, Host, 1}
+    end.
 
 record_request({ThrottleResponse, Host, ReqCount}) ->
-  erldns_storage:insert(host_throttle, {Host, {ReqCount, timestamp()}}),
-  {ThrottleResponse, Host, ReqCount}.
+    erldns_storage:insert(host_throttle, {Host, {ReqCount, timestamp()}}),
+    {ThrottleResponse, Host, ReqCount}.
 
 is_throttled({127,0,0,1}, ReqCount, _) -> {false, ReqCount + 1};
 is_throttled(Host, ReqCount, LastRequestAt) ->
-  ExceedsLimit = ReqCount >= ?LIMIT,
-  Expired = timestamp() - LastRequestAt > ?EXPIRATION,
-  case Expired of
-    true -> 
-      erldns_storage:delete(host_throttle, Host),
-      {false, 1};
-    false -> 
-      {ExceedsLimit, ReqCount + 1}
-  end.
+    ExceedsLimit = ReqCount >= ?LIMIT,
+    Expired = timestamp() - LastRequestAt > ?EXPIRATION,
+    case Expired of
+        true ->
+            erldns_storage:delete(host_throttle, Host),
+            {false, 1};
+        false ->
+            {ExceedsLimit, ReqCount + 1}
+    end.
 
 timestamp() ->
-  {TM, TS, _} = os:timestamp(),
-  (TM * 1000000) + TS.
+    {TM, TS, _} = os:timestamp(),
+    (TM * 1000000) + TS.
