87c54f12af5f52b9dfa1427f150f6582b14c3f77
Kyle Neal <kyle@verias.com>
2014-12-15 14:09:06 -0500

    (chore) #noticket Emacs reformatting, and code cleanup.

--- src/erldns_edns.erl
+++ src/erldns_edns.erl
@@ -20,15 +20,15 @@
 -export([handle/1]).
 
 handle(Message) ->
-  handle_opts(Message, Message#dns_message.additional).
+    handle_opts(Message, Message#dns_message.additional).
 
 handle_opts(Message, []) ->
-  Message;
+    Message;
 handle_opts(Message, [RR|Rest]) when is_record(RR, dns_optrr) ->
-  NewMessage = case RR#dns_optrr.dnssec of
-                 true -> erldns_dnssec:handle(Message);
-                 false -> Message
-               end,
-  handle_opts(NewMessage, Rest);
+    NewMessage = case RR#dns_optrr.dnssec of
+                     true -> erldns_dnssec:handle(Message);
+                     false -> Message
+                 end,
+    handle_opts(NewMessage, Rest);
 handle_opts(Message, [_|Rest]) ->
-  handle_opts(Message, Rest).
+    handle_opts(Message, Rest).
