e72de0ac21c674302827bb77b202413b05b2db40
Kyle Neal <kyle@verias.com>
2014-12-15 14:16:10 -0500

    (chore) #noticket Emacs reformatting, and code cleanup.

--- src/sample_custom_handler.erl
+++ src/sample_custom_handler.erl
@@ -22,7 +22,7 @@
 
 -export([start_link/0, handle/3, filter/1]).
 
-% Gen server hooks
+%% Gen server hooks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(DNS_SAMPLE_TYPE, 20001).
@@ -32,54 +32,54 @@
 %% API
 
 start_link() ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 handle(Qname, Qtype, Records) ->
-  gen_server:call(?MODULE, {handle, Qname, Qtype, Records}).
+    gen_server:call(?MODULE, {handle, Qname, Qtype, Records}).
 
 filter(Records) ->
-  gen_server:call(?MODULE, {filter, Records}).
+    gen_server:call(?MODULE, {filter, Records}).
 
 %% Gen server hooks
 init([]) ->
-  erldns_handler:register_handler([?DNS_TYPE_A], ?MODULE),
-  {ok, #state{}}.
+    erldns_handler:register_handler([?DNS_TYPE_A], ?MODULE),
+    {ok, #state{}}.
 
 handle_call({handle, _Qname, _Qtype, Records}, _From, State) ->
-  SampleRecords = lists:filter(type_match(), Records),
-  NewRecords = lists:flatten(lists:map(convert(), SampleRecords)),
-  {reply, NewRecords, State};
+    SampleRecords = lists:filter(type_match(), Records),
+    NewRecords = lists:flatten(lists:map(convert(), SampleRecords)),
+    {reply, NewRecords, State};
 
 handle_call({filter, Records}, _From, State) ->
-  TypeMatchFunction = type_match(),
-  ConvertFunction = convert(),
-  NewRecords = lists:flatten(lists:map(
-                               fun(R) ->
-                                   case TypeMatchFunction(R) of
-                                     true -> ConvertFunction(R);
-                                     false -> R
-                                   end
-                               end, Records)),
-  {reply, NewRecords, State}.
+    TypeMatchFunction = type_match(),
+    ConvertFunction = convert(),
+    NewRecords = lists:flatten(lists:map(
+                                 fun(R) ->
+                                         case TypeMatchFunction(R) of
+                                             true -> ConvertFunction(R);
+                                             false -> R
+                                         end
+                                 end, Records)),
+    {reply, NewRecords, State}.
 
 handle_cast(_Message, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 handle_info(_Message, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 terminate(_Reason, _State) ->
-  ok.
+    ok.
 
 code_change(_PreviousVersion, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
 
 %% Internal functions
 
 type_match() -> fun(Record) -> Record#dns_rr.type =:= ?DNS_SAMPLE_TYPE end.
 
-convert() -> 
-  fun(Record) ->
-      {ok, Address} = inet_parse:address(binary_to_list(Record#dns_rr.data)),
-      Record#dns_rr{type = ?DNS_TYPE_A, data = #dns_rrdata_a{ip=Address}}
-  end.
+convert() ->
+    fun(Record) ->
+            {ok, Address} = inet_parse:address(binary_to_list(Record#dns_rr.data)),
+            Record#dns_rr{type = ?DNS_TYPE_A, data = #dns_rrdata_a{ip=Address}}
+    end.
