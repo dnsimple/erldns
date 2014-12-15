6ede384db4c331a1660304fcda6bcf8a08a096ae
Kyle Neal <kyle@verias.com>
2014-12-15 14:09:20 -0500

    (chore) #noticket Emacs reformatting, and code cleanup.

--- src/erldns_encoder.erl
+++ src/erldns_encoder.erl
@@ -25,36 +25,37 @@
 %% Note that this function should never throw an exception.
 -spec encode_message(dns:message()) -> dns:message_bin().
 encode_message(Response) ->
-  encode_message(Response, []).
+    encode_message(Response, []).
 
 %% @doc Encode the DNS message into its binary representation. Use the
 %% Opts argument to pass in encoding options.
 %%
 %% Note that this function should never throw an exception.
 -spec encode_message(dns:message(), [dns:encode_message_opt()]) ->
-  {false, dns:message_bin()} |
-  {true, dns:message_bin(), dns:message()} |
-  {false, dns:message_bin(), dns:tsig_mac()} |
-  {true, dns:message_bin(), dns:tsig_mac(), dns:message()}.
+                            {false, dns:message_bin()} |
+                            {true, dns:message_bin(), dns:message()} |
+                            {false, dns:message_bin(), dns:tsig_mac()} |
+                            {true, dns:message_bin(), dns:tsig_mac(), dns:message()}.
 encode_message(Response, Opts) ->
-  case application:get_env(erldns, catch_exceptions) of
-    {ok, false} -> dns:encode_message(Response, Opts);
-    _ ->
-      try dns:encode_message(Response, Opts) of
-        M -> M
-      catch
-        Exception:Reason ->
-          erldns_log:error("Error encoding ~p (~p:~p)", [Response, Exception, Reason]),
-          encode_message(build_error_response(Response))
-      end
-  end.
+    case application:get_env(erldns, catch_exceptions) of
+        {ok, false} -> dns:encode_message(Response, Opts);
+        _ ->
+            try dns:encode_message(Response, Opts) of
+                M -> M
+            catch
+                Exception:Reason ->
+                    erldns_log:error("Error encoding ~p (~p:~p)", [Response, Exception, Reason]),
+                    encode_message(build_error_response(Response))
+            end
+    end.
 
-% Private functions
+                                                % Private functions
 
 %% Populate a response with a servfail error
 build_error_response(Response) when is_record(Response, dns_message) ->
-  build_error_response(Response, ?DNS_RCODE_SERVFAIL);
+    build_error_response(Response, ?DNS_RCODE_SERVFAIL);
 build_error_response({_, Response}) ->
-  build_error_response(Response, ?DNS_RCODE_SERVFAIL).
+    build_error_response(Response, ?DNS_RCODE_SERVFAIL).
 build_error_response(Response, Rcode) ->
-  Response#dns_message{anc = 0, auc = 0, adc = 0, qr = true, aa = true, rc = Rcode, answers=[], authority=[], additional=[]}.
+    Response#dns_message{anc = 0, auc = 0, adc = 0, qr = true, aa = true, rc = Rcode, answers=[],
+                         authority=[], additional=[]}.
