bb8d5e3ec7ab8c2e01b696bb6cee0ff842926491
Anthony Eden <me@anthonyeden.com>
2015-02-23 08:23:35 +0100

    Refactor into pattern matching functions.

--- src/erldns_dnssec_nsec_simple.erl
+++ src/erldns_dnssec_nsec_simple.erl
@@ -6,63 +6,67 @@
 -export([include_nsec/5]).
 
 include_nsec(Message, Qname, Qtype, ZoneWithRecords, _CnameChain) ->
-  Soa = lists:last(ZoneWithRecords#zone.authority),
-  SoaMinimumTtl = Soa#dns_rr.data#dns_rrdata_soa.minimum,
-  NextDname = dns:labels_to_dname([<<"\003">>] ++ dns:dname_to_labels(Qname)),
+  include_nsec(Message, Qname, Qtype, ZoneWithRecords, _CnameChain, Message#dns_message.rc).
 
-  case Message#dns_message.rc of
-    ?DNS_RCODE_NXDOMAIN ->
-      lager:debug("Zone present, but response is NXDOMAIN"),
-      case lists:any(erldns_records:match_name(Qname), ZoneWithRecords#zone.records) of
-        true ->
-          lager:debug("Name is present, but does not match qtype"),
+include_nsec(Message, Qname, Qtype, ZoneWithRecords, _CnameChain, _Rcode = ?DNS_RCODE_NXDOMAIN) ->
+  lager:debug("Zone present, but response is NXDOMAIN"),
+  include_nsec_nxdomain(Message, Qname, Qtype, ZoneWithRecords, _CnameChain, lists:filter(erldns_records:match_name(Qname), ZoneWithRecords#zone.records));
+include_nsec(Message, Qname, Qtype, ZoneWithRecords, _CnameChain, _Rcode = ?DNS_RCODE_NOERROR) ->
+  lager:debug("Zone present, response code is NOERROR"),
+  include_nsec_noerror(Message, Qname, Qtype, ZoneWithRecords, _CnameChain, lists:filter(erldns_records:match_name_and_type(Qname, Qtype), ZoneWithRecords#zone.records), lists:filter(erldns_records:match_type(Qtype), ZoneWithRecords#zone.records));
+include_nsec(Message, _Qname, _Qtype, _ZoneWithRecords, _CnameChain, Rcode) ->
+  lager:debug("Rcode was ~p", [Rcode]),
+  Message.
+
+
+include_nsec_nxdomain(Message, Qname, _Qtype, ZoneWithRecords, _CnameChain, _NameMatchedRecords = []) ->
+  lager:debug("Name and type not present"),
+  Types = [?DNS_TYPE_NSEC, ?DNS_TYPE_RRSIG],
+  NsecData = #dns_rrdata_nsec{next_dname = next_dname(Qname), types = Types},
+  NSECRecords = [#dns_rr{name = Qname, type = ?DNS_TYPE_NSEC, ttl = nsec_ttl(ZoneWithRecords), data = NsecData}],
+  Message#dns_message{authority = Message#dns_message.authority ++ NSECRecords};
+include_nsec_nxdomain(Message, _Qname, _Qtype, _ZoneWithRecords, _CnameChain, _NameMatchedRecords) ->
+  lager:debug("Name is present, but does not match qtype"),
+  Message.
+
+
+include_nsec_noerror(Message, Qname, Qtype, ZoneWithRecords, _CnameChain, _NameAndTypeMatchedRecords = [], TypeMatchedRecords) ->
+  Records = lists:filter(erldns_records:match_name(Qname), ZoneWithRecords#zone.records),
+  AdditionalTypes = lists:usort(lists:map(fun(RR) -> RR#dns_rr.type end, Records)),
+  Types = AdditionalTypes ++ [?DNS_TYPE_NSEC, ?DNS_TYPE_RRSIG],
+  NsecData = #dns_rrdata_nsec{next_dname = next_dname(Qname), types = Types},
+  NSECRecords = [#dns_rr{name = Qname, type = ?DNS_TYPE_NSEC, ttl = nsec_ttl(ZoneWithRecords), data = NsecData}],
+
+  case TypeMatchedRecords of
+    [] ->
+      case Qtype of
+        ?DNS_TYPE_ANY ->
+          lager:debug("Name is present, type is ANY"),
+          Message#dns_message{answers = Message#dns_message.answers ++ NSECRecords};
+        ?DNS_TYPE_DNSKEY ->
+          lager:debug("Name is present, type is DNSKEY"),
           Message;
-        false ->
-          lager:debug("Name and type not present"),
-
-          Types = [?DNS_TYPE_NSEC, ?DNS_TYPE_RRSIG],
-
-          NSECRecords = [#dns_rr{name = Qname, type = ?DNS_TYPE_NSEC, ttl = SoaMinimumTtl, data = #dns_rrdata_nsec{next_dname = NextDname, types = Types}}],
-
+        _ ->
+          lager:debug("Name is present, type is not"),
           Message#dns_message{authority = Message#dns_message.authority ++ NSECRecords}
       end;
-    ?DNS_RCODE_NOERROR ->
-      lager:debug("Zone present, response code is NOERROR"),
-      case lists:any(erldns_records:match_name_and_type(Qname, Qtype), ZoneWithRecords#zone.records) of
+    _ ->
+      lager:debug("Name and type do not match, but name is present with another type"),
+      case lists:any(erldns_records:match_name_and_type(Qname, ?DNS_TYPE_CNAME), ZoneWithRecords#zone.records) of
         true ->
-          lager:debug("Name and type are present"),
+          lager:debug("Other type is CNAME"),
           Message;
         false ->
-          Records = lists:filter(erldns_records:match_name(Qname), ZoneWithRecords#zone.records),
-          AdditionalTypes = lists:usort(lists:map(fun(RR) -> RR#dns_rr.type end, Records)),
-          Types = AdditionalTypes ++ [?DNS_TYPE_NSEC, ?DNS_TYPE_RRSIG],
-          NSECRecords = [#dns_rr{name = Qname, type = ?DNS_TYPE_NSEC, ttl = SoaMinimumTtl, data = #dns_rrdata_nsec{next_dname = NextDname, types = Types}}],
+          Message#dns_message{authority = Message#dns_message.authority ++ NSECRecords}
+      end
+  end;
+include_nsec_noerror(Message, _Qname, _Qtype, _ZoneWithRecords, _CnameChain, _NameAndTypeMatchedRecords, _TypeMatchedRecords) ->
+  lager:debug("Name and type are present"),
+  Message.
 
-          case lists:any(erldns_records:match_type(Qtype), ZoneWithRecords#zone.records) of
-            true ->
-              lager:debug("Name and type do not match, but name is present with another type"),
-              case lists:any(erldns_records:match_name_and_type(Qname, ?DNS_TYPE_CNAME), ZoneWithRecords#zone.records) of
-                true ->
-                  lager:debug("Other type is CNAME"),
-                  Message;
-                false ->
-                  Message#dns_message{authority = Message#dns_message.authority ++ NSECRecords}
-              end;
-            false ->
-              case Qtype of
-                ?DNS_TYPE_ANY ->
-                  lager:debug("Name is present, type is ANY"),
-                  Message#dns_message{answers = Message#dns_message.answers ++ NSECRecords};
-                ?DNS_TYPE_DNSKEY ->
-                  lager:debug("Name is present, type is DNSKEY"),
-                  Message;
-                _ ->
-                  lager:debug("Name is present, type is not"),
-                  Message#dns_message{authority = Message#dns_message.authority ++ NSECRecords}
-              end
-          end
-      end;
-    Rcode ->
-      lager:debug("Rcode was ~p", [Rcode]),
-      Message
-  end.
+next_dname(Qname) ->
+  dns:labels_to_dname([<<"\003">>] ++ dns:dname_to_labels(Qname)).
+
+nsec_ttl(Zone) ->
+  Soa = lists:last(Zone#zone.authority),
+  Soa#dns_rr.data#dns_rrdata_soa.minimum.
