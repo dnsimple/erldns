38a40b5eb1428e5ed09c32036812a3cf1f8ee192
Anthony Eden <me@anthonyeden.com>
2015-03-02 17:03:25 +0100

    Measure empty responses.
    In normal operations this should not happen, however
    during the recent attempted release of the (disabled)
    DNSSEC implementation some resolvers were reporting
    empty responses. I want to verify that this is 
    not coming from erldns.

--- src/erldns_event_handler.erl
+++ src/erldns_event_handler.erl
@@ -64,6 +64,12 @@ handle_event({tcp_error, Reason}, State) ->
   folsom_metrics:notify({tcp_error_history, Reason}),
   {ok, State};
 
+handle_event({empty_response, Message}, State) ->
+  folsom_metrics:notify({empty_response_meter, 1}),
+  folsom_metrics:notify({empty_response_counter, {inc, 1}}),
+  folsom_metrics:notify({empty_response_history, Message}),
+  {ok, State};
+
 handle_event(_Event, State) ->
   {ok, State}.
 
