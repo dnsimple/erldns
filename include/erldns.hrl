6b49f6aa19dd53e7c1a2b1942b3a1db396a72608
Sargun Dhillon <sargun@sargun.me>
2016-09-06 11:18:25 +0200

    Indicate deprecation of #zone.records_by_type field and update typespec

--- include/erldns.hrl
+++ include/erldns.hrl
@@ -7,7 +7,9 @@
     record_count = 0 :: non_neg_integer(),
     records = [] :: [dns:rr()],
     records_by_name ::  dict:dict(binary(), [#dns_rr{}]) | trimmed,
-    records_by_type :: [dns:rr()]
+    %% records_by_type is no longer in use, but cannot (easily) be deleted due to Mnesia schema evolution
+    %% We cannot set it to undefined, because, again, when fetched from Mnesia, it may be set
+    records_by_type :: term()
   }).
 
 -record(authorities, {
