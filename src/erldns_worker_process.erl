bb1469fc48c85c3269ff4dbfb81acf4e52fd92b8
Ivan Bakalov <ivan.bakalov@dnsimple.com>
2021-09-14 15:34:16 +0300

    Open telemetry tracer (#132)
    Adds OpenTelemetry (OTEL) instrumentation for the read operations within the application.

--- src/erldns_worker_process.erl
+++ src/erldns_worker_process.erl
@@ -16,6 +16,7 @@
 -module(erldns_worker_process).
 
 -include_lib("dns_erlang/include/dns.hrl").
+-include_lib("opentelemetry_api/include/otel_tracer.hrl").
 
 -behaviour(gen_server).
 
@@ -50,23 +51,58 @@ init(_Args) ->
     {ok, #state{}}.
 
 % Process a TCP request. Does not truncate the response.
-handle_call({process, DecodedMessage, Socket, {tcp, Address}}, _From, State) ->
+handle_call({process, DecodedMessage, Socket, {tcp, Address}, SpanCtx}, _From, State) ->
+    ?set_current_span(SpanCtx),
     % Uncomment this and the function implementation to simulate a timeout when
     % querying www.example.com with the test zones
     % simulate_timeout(DecodedMessage),
-    Response = erldns_handler:handle(DecodedMessage, {tcp, Address}),
-    EncodedMessage = erldns_encoder:encode_message(Response),
-    send_tcp_message(Socket, EncodedMessage),
+    Response = ?with_span(<<"synthesize_answer">>, #{},
+        fun(_SpanCtx) ->
+            erldns_handler:handle(DecodedMessage, {tcp, Address})
+        end
+    ),
+    EncodedMessage = ?with_span(<<"encode_message">>, #{},
+        fun(_SpanCtx) ->
+            ?set_attributes([
+                {<<"rcode">>, Response#dns_message.rc},
+                {<<"aa">>, Response#dns_message.aa},
+                {<<"ra">>, Response#dns_message.ra},
+                {<<"answers">>, length(Response#dns_message.answers)}
+            ]),
+            erldns_encoder:encode_message(Response)
+        end
+    ),
+    ?with_span(<<"send_tcp_message">>, #{},
+        fun(_SpanCtx) ->
+            send_tcp_message(Socket, EncodedMessage)
+        end
+    ),
     {reply, ok, State};
 % Process a UDP request. May truncate the response.
-handle_call({process, DecodedMessage, Socket, Port, {udp, Host}}, _From, State) ->
+handle_call({process, DecodedMessage, Socket, Port, {udp, Host}, SpanCtx}, _From, State) ->
+    ?set_current_span(SpanCtx),
     % Uncomment this and the function implementation to simulate a timeout when
     % querying www.example.com with the test zones
     % simulate_timeout(DecodedMessage),
-    Response = erldns_handler:handle(DecodedMessage, {udp, Host}),
+    Response = ?with_span(<<"synthesize_answer">>, #{},
+                    fun(_SpanCtx) ->
+                        erldns_handler:handle(DecodedMessage, {udp, Host})
+                    end
+                ),
     DestHost = ?DEST_HOST(Host),
 
-    case erldns_encoder:encode_message(Response, [{max_size, max_payload_size(Response)}]) of
+    Result = ?with_span(<<"encode_message">>, #{},
+                 fun(_SpanCtx) ->
+                    ?set_attributes([
+                        {<<"rcode">>, Response#dns_message.rc},
+                        {<<"aa">>, Response#dns_message.aa},
+                        {<<"ra">>, Response#dns_message.ra},
+                        {<<"answers">>, length(Response#dns_message.answers)}
+                    ]),
+                     erldns_encoder:encode_message(Response, [{max_size, max_payload_size(Response)}])
+                 end
+             ),
+    case Result of
         {false, EncodedMessage} ->
             % lager:debug("Sending encoded response to ~p", [DestHost]),
             gen_udp:send(Socket, DestHost, Port, EncodedMessage);
