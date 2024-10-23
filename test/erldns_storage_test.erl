49c9fd494cdc841ee4c6f13c5c5ebbf408d4732a
Andrea Leopardi <an.leopardi@gmail.com>
2024-10-23 20:56:41 +0200

    Formatting

--- test/erldns_storage_test.erl
+++ test/erldns_storage_test.erl
@@ -14,27 +14,10 @@ load_zones_with_existing_file_test() ->
     erldns_storage:create(zone_records_typed),
     Filename = filename:join(tmp_dir(), "zones.json"),
 
-    ok = file:write_file(Filename, """
-        [{
-            "name": "example.com",
-            "records": [
-                {
-                    "name": "example.com",
-                    "type": "SOA",
-                    "data": {
-                    "mname": "ns1.example.com",
-                    "rname": "ahu.example.com",
-                    "serial": 2000081501,
-                    "refresh": 28800,
-                    "retry": 7200,
-                    "expire": 604800,
-                    "minimum": 86400
-                    },
-                    "ttl": 100000
-                }
-            ]
-        }]
-        """),
+    ok = file:write_file(
+        Filename,
+        <<"[{\"name\": \"example.com\", \"records\": [{\"name\": \"example.com\", \"type\": \"SOA\", \"data\": {\"mname\": \"ns1.example.com\", \"rname\": \"ahu.example.com\", \"serial\": 2000081501, \"refresh\": 28800, \"retry\": 7200, \"expire\": 604800, \"minimum\": 86400}, \"ttl\": 100000}]}]">>
+    ),
 
     ?assertMatch({ok, 1}, ?TEST_MODULE:load_zones(Filename)),
     gen_server:stop(Pid).
