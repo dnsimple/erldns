e6ba36def5fb5157711ae308f58a1d4392251c19
Andrea Leopardi <an.leopardi@gmail.com>
2024-10-28 13:02:54 +0100

    WIP: check dependencies

---
+++ .github/workflows/check-deps.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z "$BRANCH_NAME" ]; then
+    echo "Branch name is required"
+    exit 1
+fi
+
+function check_pr {
+    gh pr list --state open --label "update dependencies"
+}
+
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+_update_output="$(rebar3 update-deps --replace)"
+
+if git diff --exit-code --quiet; then
+    echo "No changes to the dependencies"
+    exit 0
+fi
+
+git checkout -b "$BRANCH_NAME"
+git add .
+git commit -m "Update dependencies" -m "Updates from \"rebar3 update-deps --replace\":\n\n$_update_output"
+
+git push --set-upstream origin "$BRANCH_NAME"
+
+if [[ $(check_pr) == "" ]]; then
+    gh pr create --fill --label "update dependencies"
+fi
