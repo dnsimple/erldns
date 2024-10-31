cf99185e66cdc52e8bc80d6badc48929f905a3fd
Andrea Leopardi <an.leopardi@gmail.com>
2024-10-31 10:46:17 +0100

    Fix script

--- .github/workflows/check-deps.sh
+++ .github/workflows/check-deps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 GITHUB_PR_LABEL="dependencies"
+COMMIT_TITLE="Update dependencies"
 
 if [ -z "$BRANCH_NAME" ]; then
     echo "Branch name is required"
@@ -19,13 +20,15 @@ _update_output="$(TERM=dumb rebar3 update-deps --replace)"
 if git diff --exit-code --quiet; then
     echo "No changes to the dependencies"
     exit 0
+else
+    echo "Detected changes to dependencies"
 fi
 
 git checkout -b "$BRANCH_NAME"
 git add .
 
 git commit -F- <<EOF
-Update dependencies
+$COMMIT_TITLE
 
 Updates from "rebar3 update-deps --replace":
 
@@ -35,5 +38,16 @@ EOF
 git push --set-upstream origin "$BRANCH_NAME"
 
 if [[ $(check_pr) == "" ]]; then
-    gh pr create --fill --label "$GITHUB_PR_LABEL"
+    gh pr create \
+        --label "$GITHUB_PR_LABEL" \
+        --title "$COMMIT_TITLE" \
+        --body-file - <<EOF
+This PR updates the dependencies to their latest versions.
+
+The following changes were made from \`rebar3 update-deps --replace\`:
+
+\`\`\`
+$_update_output
+\`\`\`
+EOF
 fi
