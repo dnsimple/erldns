a428c396220dcd7bfe1b6db0a4c6b7c85347c1f4
Nelson Vides <nelson.vides@erlang-solutions.com>
2025-04-10 14:00:16 +0200

    Add tests for admin endpoint and fix bugs

---
+++ test/admin_endpoint_SUITE.erl
@@ -0,0 +1,245 @@
+-module(admin_endpoint_SUITE).
+-compile([export_all, nowarn_export_all]).
+
+-behaviour(ct_suite).
+
+-include_lib("stdlib/include/assert.hrl").
+
+-spec all() -> [ct_suite:ct_test_def()].
+all() ->
+    [{group, all}].
+
+-spec groups() -> [ct_suite:ct_group_def()].
+groups() ->
+    [
+        {all, [parallel], [
+            bad_auth,
+            get_zones,
+            get_not_found_resource,
+            get_zone_resources,
+            get_zone_resources_with_metaonly,
+            fake_action_zone,
+            get_zone_record_resource,
+            get_zone_record_resource_name,
+            get_zone_record_resource_name_type,
+            get_zone_record_resource_name_type_non_existing
+        ]}
+    ].
+
+-spec init_per_suite(ct_suite:ct_config()) -> ct_suite:ct_config().
+init_per_suite(Config) ->
+    Servers = [
+        [
+            {name, inet_localhost_1},
+            {address, "127.0.0.1"},
+            {port, 8053},
+            {family, inet},
+            {processes, 10}
+        ]
+    ],
+    AdminPort = 8083,
+    AppConfig = [
+        {erldns, [
+            {servers, Servers},
+            {ff_use_txts_field, true},
+            {admin, [
+                {credentials, {<<"username">>, <<"password">>}},
+                {port, AdminPort}
+            ]}
+        ]}
+    ],
+    application:set_env(AppConfig),
+    {ok, _} = application:ensure_all_started([erldns, ssl, inets]),
+    FileName = filename:join([code:priv_dir(erldns), "zones-example.json"]),
+    {ok, _} = erldns_storage:load_zones(FileName),
+    [{port, AdminPort} | Config].
+
+-spec end_per_suite(ct_suite:ct_config()) -> term().
+end_per_suite(_) ->
+    application:stop(erldns).
+
+-spec init_per_group(ct_suite:ct_groupname(), ct_suite:ct_config()) -> ct_suite:ct_config().
+init_per_group(_, Config) ->
+    Config.
+
+-spec end_per_group(ct_suite:ct_groupname(), ct_suite:ct_config()) -> term().
+end_per_group(_, _Config) ->
+    ok.
+
+-spec init_per_testcase(ct_suite:ct_testcase(), ct_suite:ct_config()) -> ct_suite:ct_config().
+init_per_testcase(_, Config) ->
+    Config.
+
+-spec end_per_testcase(ct_suite:ct_testcase(), ct_suite:ct_config()) -> term().
+end_per_testcase(_, Config) ->
+    Config.
+
+%% Tests
+bad_auth(CtConfig) ->
+    Request = {endpoint(CtConfig, ""), headers(bad_auth)},
+    case httpc:request(get, Request, [], []) of
+        {_, {{_Version, 401, "Unauthorized"}, _Headers, _Body}} ->
+            ok;
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+get_zones(CtConfig) ->
+    Request = {endpoint(CtConfig, ""), headers(good)},
+    case httpc:request(get, Request, [], []) of
+        {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Payload}} ->
+            Body = json:decode(iolist_to_binary(Payload)),
+            ?assertMatch(
+                #{
+                    <<"erldns">> :=
+                        #{
+                            <<"zones">> :=
+                                #{<<"count">> := 1, <<"versions">> := _}
+                        }
+                },
+                Body
+            );
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+get_not_found_resource(CtConfig) ->
+    Request = {endpoint(CtConfig, "/zones/non_existing_zone.bad"), headers(good)},
+    case httpc:request(get, Request, [], []) of
+        {ok, {{_Version, 404, "Not Found"}, _Headers, _Payload}} ->
+            ok;
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+get_zone_resources(CtConfig) ->
+    Request = {endpoint(CtConfig, "/zones/example.com"), headers(good)},
+    case httpc:request(get, Request, [], []) of
+        {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Payload}} ->
+            Body = json:decode(iolist_to_binary(Payload)),
+            ?assertMatch(
+                #{
+                    <<"erldns">> :=
+                        #{
+                            <<"zone">> :=
+                                #{
+                                    <<"name">> := <<"example.com">>,
+                                    <<"records">> := _,
+                                    <<"records_count">> := 11,
+                                    <<"version">> := <<>>
+                                }
+                        }
+                },
+                Body
+            );
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+get_zone_resources_with_metaonly(CtConfig) ->
+    Request = {endpoint(CtConfig, "/zones/example.com?metaonly=true"), headers(good)},
+    case httpc:request(get, Request, [], []) of
+        {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Payload}} ->
+            Body = json:decode(iolist_to_binary(Payload)),
+            ?assertMatch(
+                #{
+                    <<"erldns">> :=
+                        #{
+                            <<"zone">> :=
+                                #{
+                                    <<"name">> := <<"example.com">>,
+                                    <<"records_count">> := 11,
+                                    <<"version">> := <<>>
+                                }
+                        }
+                },
+                Body
+            );
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+fake_action_zone(CtConfig) ->
+    Request = {endpoint(CtConfig, "/zones/example.com/get"), headers(good)},
+    case httpc:request(get, Request, [], []) of
+        {ok, {{_Version, 200, _ReasonPhrase}, _Headers, ""}} ->
+            ok;
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+% {"/zones/:zone_name/records[/:record_name]", erldns_admin_zone_records_resource_handler, State}
+get_zone_record_resource(CtConfig) ->
+    Request = {endpoint(CtConfig, "/zones/example.com/records"), headers(good)},
+    case httpc:request(get, Request, [], []) of
+        {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Payload}} ->
+            Body = json:decode(iolist_to_binary(Payload)),
+            ?assertMatch([], Body);
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+get_zone_record_resource_name(CtConfig) ->
+    Request = {endpoint(CtConfig, "/zones/example.com/records/www.example.com"), headers(good)},
+    case httpc:request(get, Request, [], []) of
+        {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Payload}} ->
+            Body = json:decode(iolist_to_binary(Payload)),
+            ?assertMatch(
+                [
+                    #{
+                        <<"content">> := <<"example.com.">>,
+                        <<"name">> := <<"www.example.com.">>,
+                        <<"ttl">> := 120,
+                        <<"type">> := <<"A">>
+                    }
+                ],
+                Body
+            );
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+get_zone_record_resource_name_type(CtConfig) ->
+    Request = {endpoint(CtConfig, "/zones/example.com/records/www.example.com?type=CNAME"), headers(good)},
+    case httpc:request(get, Request, [], []) of
+        {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Payload}} ->
+            Body = json:decode(iolist_to_binary(Payload)),
+            ?assertMatch(
+                [
+                    #{
+                        <<"content">> := <<"example.com.">>,
+                        <<"name">> := <<"www.example.com.">>,
+                        <<"ttl">> := 120,
+                        <<"type">> := <<"A">>
+                    }
+                ],
+                Body
+            );
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+get_zone_record_resource_name_type_non_existing(CtConfig) ->
+    Request = {endpoint(CtConfig, "/zones/example.com/records/www.example.com?type=A"), headers(good)},
+    case httpc:request(get, Request, [], []) of
+        {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Payload}} ->
+            Body = json:decode(iolist_to_binary(Payload)),
+            ?assertMatch([], Body);
+        {_, Other} ->
+            ct:fail(Other)
+    end.
+
+endpoint(CtConfig, Path) ->
+    Port = proplists:get_value(port, CtConfig),
+    "http://localhost:" ++ integer_to_list(Port) ++ Path.
+
+headers(good) ->
+    [
+        {"accept", "application/json"},
+        {"authorization", "basic " ++ base64:encode("username:password")}
+    ];
+headers(bad_auth) ->
+    [
+        {"accept", "application/json"},
+        {"authorization", "basic " ++ base64:encode("bad_username:bad_password")}
+    ].
